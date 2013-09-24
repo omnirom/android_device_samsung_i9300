054e5692a9ac3c1abafc7c8ccdc98f9f937e9ef6
Dominik Marszk <dmarszk@gmail.com>
2013-09-24 21:39:31 +0100

    [WIP] i9300: Initial bringup - making it build in Omni build env.
    It boots up properly with most of the functionallity.
    What's missing currently is GPS, TvOut, FM and BT.
    
    PS4 - added proper custom config with Omni specific inherits.
    It brunches now!
    
    PS5 - preparation for omni vendor config, it won't build just yet.
    If you want to build it - use PS4.
    
    Change-Id: Ic5c31796cbfb73470caf678b199ac1329e30c5b7

---
+++ aosp_i9300.mk
@@ -0,0 +1,3 @@
+$(call inherit-product, device/samsung/i9300/full_i9300.mk)
+
+PRODUCT_NAME := aosp_i9300
--- omni_i9300.mk
+++ omni_i9300.mk
@@ -14,14 +14,17 @@
 # limitations under the License.
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/i9300/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_i9300
+PRODUCT_NAME := omni_i9300
 PRODUCT_DEVICE := i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
