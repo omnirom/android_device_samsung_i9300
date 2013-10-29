09cfdfaa9b253f59c3b91cea5fe9c5b5adef5987
Dominik Marszk <dmarszk@gmail.com>
2013-10-29 18:14:39 +0000

    defconfigs: Debranding
    Change-Id: I19e0859b33d8469ed42303ec7db3e4a01a603f0c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
+TARGET_KERNEL_CONFIG := custom_i9300_defconfig
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9300/rootdir/fstab.smdk4x12
