71fbd02d564117468d03f9cb8478ecb0300fefcb
Markus Guidry <markus@gu1dry.com>
2014-03-09 14:45:21 -0400

    i9300: Align ro.product with stock
    Additionally adds a stock fingerprint (I9300XXUGNA7) & a README.
    This also removes the unused aosp_i9300.mk & full_i9300.mk.
    
      PS2: More correction in AndroidProducts.mk in regard to the
           removal of aosp_i9300.mk.
      PS3: Forgot to also remove full from vendorsetup
      PS4: ect -> etc in a comment
    
    Change-Id: I95c90ac806b8092bac0f380742ea2e4055bd0657

--- omni_i9300.mk
+++ omni_i9300.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2013 OmniROM Project
 # Copyright (C) 2012 The CyanogenMod Project
 #
@@ -12,23 +13,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from our omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/i9300/device.mk)
 
-# Discard inherited values and use our own instead.
+# Device identifier. This must come after all inclusions
+PRODUCT_MODEL := GT-I9300
+PRODUCT_BRAND := samsung
 PRODUCT_NAME := omni_i9300
 PRODUCT_DEVICE := i9300
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-I9300
+PRODUCT_MANUFACTURER := samsung
+
+#Set build fingerprint / ID / Prduct Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m0xx \
+    TARGET_DEVICE=m0 \
+    PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGNA7 release-keys" \
+    BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGNA7:user/release-keys"
