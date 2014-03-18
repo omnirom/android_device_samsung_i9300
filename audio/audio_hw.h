413b27289dc8bc12fec1b8b66f11dbdf5c3dfe25
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-03-18 13:17:00 +0530

    i9300: update overlays
    - Enable noise suppression
    - Fix camera bugs - can't connect to camera
    - Delete obsolete 4.3 & focal files
    - Add 4.4 files
    - Whitespace & indentation cleanup
    - Remove some overlays which have default value
    
    Change-Id: I6976a24f39301f2f4f0c0f2fd4f9d55e569fd7f5

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -141,6 +141,7 @@ struct route_setting voicecall_default_disable[] = {
 
 struct route_setting default_input[] = {
     { .ctl_name = "Main Mic Switch", .intval = 1, },
+    { .ctl_name = "MainMicBias Mode", .intval = 1, },
     { .ctl_name = "IN1L Volume", .intval = 28, },
     { .ctl_name = "MIXINL IN1L Switch", .intval = 1, },
     { .ctl_name = "MIXINL IN1L Volume", .intval = 0, },
