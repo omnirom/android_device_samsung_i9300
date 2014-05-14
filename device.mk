bc0c32b832071b105e9fd00517b34879b8f3ad3c
Javier Ferrer <javi.f.o@gmail.com>
2014-05-14 23:36:36 +0200

    Camera Wrapper
    Change-Id: I4bb7dcead42c0f710818c6f973d83c5adab8891e

--- device.mk
+++ device.mk
@@ -38,9 +38,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/m0
 
-# Camera: Enable for source-built camera HAL
-# PRODUCT_PACKAGES += \
-#     camera.smdk4x12
+# Camera Wrapper
+PRODUCT_PACKAGES += \
+    camera.exynos4
 
 # Sensors
 PRODUCT_PACKAGES += \
