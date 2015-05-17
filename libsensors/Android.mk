dc91f14ffa2f9ccb3bc2f73ebc480f9986fe1b10
Paul Kocialkowski <contact@paulk.fr>
2015-05-17 20:20:29 +0200

    i9300: sync sensor hal with replicant
    Change-Id: I23ea9128daea17c189d0b162110c50f2cc50f8e1

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2013 Paul Kocialkowski
+# Copyright (C) 2013 Paul Kocialkowski <contact@paulk.fr>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,15 +12,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 LOCAL_PATH := $(call my-dir)
-EXYNOS_SENSORS_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	exynos_sensors.c \
+	smdk4x12_sensors.c \
 	input.c \
 	orientation.c \
 	akm8975.c \
@@ -35,7 +32,7 @@ LOCAL_SRC_FILES := \
 	cm36651_light.c \
 	lsm330dlc_acceleration.c \
 	lsm330dlc_gyroscope.c \
-	lps331ap.c \
+	lps331ap.c
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/akmdfs \
@@ -45,7 +42,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libhardware
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_MODULE := sensors.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE := sensors.smdk4x12
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
--- libsensors/smdk4x12_sensors.c
+++ libsensors/smdk4x12_sensors.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Paul Kocialkowski
+ * Copyright (C) 2013 Paul Kocialkowski <contact@paulk.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,52 +21,46 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <poll.h>
-
-
-
 #include <sys/select.h>
-
-
-
 #include <hardware/sensors.h>
 #include <hardware/hardware.h>
 
-#define LOG_TAG "exynos_sensors"
+#define LOG_TAG "smdk4x12_sensors"
 #include <utils/Log.h>
 
-#include "exynos_sensors.h"
+#include "smdk4x12_sensors.h"
 
 /*
  * Sensors list
  */
 
-struct sensor_t exynos_sensors[] = {
+struct sensor_t smdk4x12_sensors[] = {
 	{ "LSM330DLC Acceleration Sensor", "STMicroelectronics", 1, SENSOR_TYPE_ACCELEROMETER,
-		SENSOR_TYPE_ACCELEROMETER, 19.61f, 0.0096f, 0.23f, 10000, 0, 0, 0, 0, 0,
+		SENSOR_TYPE_ACCELEROMETER, 2 * GRAVITY_EARTH, 0.0096f, 0.23f, 10000, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
-	{ "AKM8975 Magnetic Sensor", "Asahi Kasei", 1, SENSOR_TYPE_MAGNETIC_FIELD,
-		SENSOR_TYPE_MAGNETIC_FIELD, 2000.0f, 0.06f, 6.8f, 10000, 0, 0, 0, 0, 0,
+	{ "AKM8975 Magnetic Sensor", "Asahi Kasei Microdevices", 1, SENSOR_TYPE_MAGNETIC_FIELD,
+		SENSOR_TYPE_MAGNETIC_FIELD, 2000.0f, 1.0f / 16, 6.8f, 10000, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
-	{ "Orientation Sensor", "Exynos Sensors", 1, SENSOR_TYPE_ORIENTATION,
+	{ "Orientation Sensor", "SMDK4x12 Sensors", 1, SENSOR_TYPE_ORIENTATION,
 		SENSOR_TYPE_ORIENTATION, 360.0f, 0.1f, 0.0f, 10000, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
-	{ "CM36651 Light Sensor", "Capella", 1, SENSOR_TYPE_LIGHT,
+	{ "CM36651 Light Sensor", "Capella Microsystems", 1, SENSOR_TYPE_LIGHT,
 		SENSOR_TYPE_LIGHT, 121240.0f, 1.0f, 0.2f, 0, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
-	{ "CM36651 Proximity Sensor", "Capella", 1, SENSOR_TYPE_PROXIMITY,
-		SENSOR_TYPE_PROXIMITY, 8.0f, 8.0f, 1.3f, 0, 0, 0, 0, 0, 0,
+	{ "CM36651 Proximity Sensor", "Capella Microsystems", 1, SENSOR_TYPE_PROXIMITY,
+		SENSOR_TYPE_PROXIMITY, 6.0f, 6.0f, 1.3f, 0, 0, 0, 0, 0, 0,
         SENSOR_FLAG_WAKE_UP | SENSOR_FLAG_ON_CHANGE_MODE, {0}, },
 	{ "LSM330DLC Gyroscope Sensor", "STMicroelectronics", 1, SENSOR_TYPE_GYROSCOPE,
-		SENSOR_TYPE_GYROSCOPE, 8.73f, 0.00031f, 6.1f, 5000, 0, 0, 0, 0, 0,
+		SENSOR_TYPE_GYROSCOPE, 500.0f * (3.1415926535f / 180.0f), (70.0f / 4000.0f) * (3.1415926535f / 180.0f), 6.1f, 5000, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
 	{ "LPS331AP Pressure Sensor", "STMicroelectronics", 1, SENSOR_TYPE_PRESSURE,
-		SENSOR_TYPE_PRESSURE, 1260.0f, 0.00024f, 0.045f, 40000, 0, 0, 0, 0, 0,
+		SENSOR_TYPE_PRESSURE, 1260.0f, 1.0f / 4096, 0.045f, 40000, 0, 0, 0, 0, 0,
         SENSOR_FLAG_CONTINUOUS_MODE, {0}, },
 };
 
-int exynos_sensors_count = sizeof(exynos_sensors) / sizeof(struct sensor_t);
+int smdk4x12_sensors_count = sizeof(smdk4x12_sensors) / sizeof(struct sensor_t);
 
-struct exynos_sensors_handlers *exynos_sensors_handlers[] = {
+struct smdk4x12_sensors_handlers *smdk4x12_sensors_handlers[] = {
 	&lsm330dlc_acceleration,
 	&akm8975,
 	&orientation,
@@ -76,16 +70,17 @@ struct exynos_sensors_handlers *exynos_sensors_handlers[] = {
 	&lps331ap,
 };
 
-int exynos_sensors_handlers_count = sizeof(exynos_sensors_handlers) /
-	sizeof(struct exynos_sensors_handlers *);
+int smdk4x12_sensors_handlers_count = sizeof(smdk4x12_sensors_handlers) /
+	sizeof(struct smdk4x12_sensors_handlers *);
 
 /*
- * Exynos Sensors
+ * SMDK4x12 Sensors
  */
 
-int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int enabled)
+int smdk4x12_sensors_activate(struct sensors_poll_device_t *dev, int handle,
+	int enabled)
 {
-	struct exynos_sensors_device *device;
+	struct smdk4x12_sensors_device *device;
 	int i;
 
 	ALOGD("%s(%p, %d, %d)", __func__, dev, handle, enabled);
@@ -93,7 +88,7 @@ int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int e
 	if (dev == NULL)
 		return -EINVAL;
 
-	device = (struct exynos_sensors_device *) dev;
+	device = (struct smdk4x12_sensors_device *) dev;
 
 	if (device->handlers == NULL || device->handlers_count <= 0)
 		return -EINVAL;
@@ -104,13 +99,13 @@ int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int e
 
 		if (device->handlers[i]->handle == handle) {
 			if (enabled && device->handlers[i]->activate != NULL) {
-				device->handlers[i]->needed |= EXYNOS_SENSORS_NEEDED_API;
-				if (device->handlers[i]->needed == EXYNOS_SENSORS_NEEDED_API)
+				device->handlers[i]->needed |= SMDK4x12_SENSORS_NEEDED_API;
+				if (device->handlers[i]->needed == SMDK4x12_SENSORS_NEEDED_API)
 					return device->handlers[i]->activate(device->handlers[i]);
 				else
 					return 0;
 			} else if (!enabled && device->handlers[i]->deactivate != NULL) {
-				device->handlers[i]->needed &= ~EXYNOS_SENSORS_NEEDED_API;
+				device->handlers[i]->needed &= ~SMDK4x12_SENSORS_NEEDED_API;
 				if (device->handlers[i]->needed == 0)
 					return device->handlers[i]->deactivate(device->handlers[i]);
 				else
@@ -122,9 +117,10 @@ int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int e
 	return -1;
 }
 
-int exynos_sensors_set_delay(struct sensors_poll_device_t *dev, int handle, int64_t ns)
+int smdk4x12_sensors_set_delay(struct sensors_poll_device_t *dev, int handle,
+	int64_t ns)
 {
-	struct exynos_sensors_device *device;
+	struct smdk4x12_sensors_device *device;
 	int i;
 
 	ALOGD("%s(%p, %d, %ld)", __func__, dev, handle, (long int) ns);
@@ -132,7 +128,7 @@ int exynos_sensors_set_delay(struct sensors_poll_device_t *dev, int handle, int6
 	if (dev == NULL)
 		return -EINVAL;
 
-	device = (struct exynos_sensors_device *) dev;
+	device = (struct smdk4x12_sensors_device *) dev;
 
 	if (device->handlers == NULL || device->handlers_count <= 0)
 		return -EINVAL;
@@ -148,10 +144,10 @@ int exynos_sensors_set_delay(struct sensors_poll_device_t *dev, int handle, int6
 	return 0;
 }
 
-int exynos_sensors_poll(struct sensors_poll_device_t *dev,
+int smdk4x12_sensors_poll(struct sensors_poll_device_t *dev,
 	struct sensors_event_t* data, int count)
 {
-	struct exynos_sensors_device *device;
+	struct smdk4x12_sensors_device *device;
 	int i, j;
 	int c, n;
 	int poll_rc, rc;
@@ -161,7 +157,7 @@ int exynos_sensors_poll(struct sensors_poll_device_t *dev,
 	if (dev == NULL)
 		return -EINVAL;
 
-	device = (struct exynos_sensors_device *) dev;
+	device = (struct smdk4x12_sensors_device *) dev;
 
 	if (device->handlers == NULL || device->handlers_count <= 0 ||
 		device->poll_fds == NULL || device->poll_fds_count <= 0)
@@ -201,9 +197,9 @@ int exynos_sensors_poll(struct sensors_poll_device_t *dev,
  * Interface
  */
 
-int exynos_sensors_close(hw_device_t *device)
+int smdk4x12_sensors_close(hw_device_t *device)
 {
-	struct exynos_sensors_device *exynos_sensors_device;
+	struct smdk4x12_sensors_device *smdk4x12_sensors_device;
 	int i;
 
 	ALOGD("%s(%p)", __func__, device);
@@ -211,16 +207,16 @@ int exynos_sensors_close(hw_device_t *device)
 	if (device == NULL)
 		return -EINVAL;
 
-	exynos_sensors_device = (struct exynos_sensors_device *) device;
+	smdk4x12_sensors_device = (struct smdk4x12_sensors_device *) device;
 
-	if (exynos_sensors_device->poll_fds != NULL)
-		free(exynos_sensors_device->poll_fds);
+	if (smdk4x12_sensors_device->poll_fds != NULL)
+		free(smdk4x12_sensors_device->poll_fds);
 
-	for (i = 0; i < exynos_sensors_device->handlers_count; i++) {
-		if (exynos_sensors_device->handlers[i] == NULL || exynos_sensors_device->handlers[i]->deinit == NULL)
+	for (i = 0; i < smdk4x12_sensors_device->handlers_count; i++) {
+		if (smdk4x12_sensors_device->handlers[i] == NULL || smdk4x12_sensors_device->handlers[i]->deinit == NULL)
 			continue;
 
-		exynos_sensors_device->handlers[i]->deinit(exynos_sensors_device->handlers[i]);
+		smdk4x12_sensors_device->handlers[i]->deinit(smdk4x12_sensors_device->handlers[i]);
 	}
 
 	free(device);
@@ -228,10 +224,10 @@ int exynos_sensors_close(hw_device_t *device)
 	return 0;
 }
 
-int exynos_sensors_open(const struct hw_module_t* module, const char *id,
+int smdk4x12_sensors_open(const struct hw_module_t* module, const char *id,
 	struct hw_device_t** device)
 {
-	struct exynos_sensors_device *exynos_sensors_device;
+	struct smdk4x12_sensors_device *smdk4x12_sensors_device;
 	int p, i;
 
 	ALOGD("%s(%p, %s, %p)", __func__, module, id, device);
@@ -239,41 +235,41 @@ int exynos_sensors_open(const struct hw_module_t* module, const char *id,
 	if (module == NULL || device == NULL)
 		return -EINVAL;
 
-	exynos_sensors_device = (struct exynos_sensors_device *)
-		calloc(1, sizeof(struct exynos_sensors_device));
-	exynos_sensors_device->device.common.tag = HARDWARE_DEVICE_TAG;
-	exynos_sensors_device->device.common.version = 0;
-	exynos_sensors_device->device.common.module = (struct hw_module_t *) module;
-	exynos_sensors_device->device.common.close = exynos_sensors_close;
-	exynos_sensors_device->device.activate = exynos_sensors_activate;
-	exynos_sensors_device->device.setDelay = exynos_sensors_set_delay;
-	exynos_sensors_device->device.poll = exynos_sensors_poll;
-	exynos_sensors_device->handlers = exynos_sensors_handlers;
-	exynos_sensors_device->handlers_count = exynos_sensors_handlers_count;
-	exynos_sensors_device->poll_fds = (struct pollfd *)
-		calloc(1, exynos_sensors_handlers_count * sizeof(struct pollfd));
+	smdk4x12_sensors_device = (struct smdk4x12_sensors_device *)
+		calloc(1, sizeof(struct smdk4x12_sensors_device));
+	smdk4x12_sensors_device->device.common.tag = HARDWARE_DEVICE_TAG;
+	smdk4x12_sensors_device->device.common.version = 0;
+	smdk4x12_sensors_device->device.common.module = (struct hw_module_t *) module;
+	smdk4x12_sensors_device->device.common.close = smdk4x12_sensors_close;
+	smdk4x12_sensors_device->device.activate = smdk4x12_sensors_activate;
+	smdk4x12_sensors_device->device.setDelay = smdk4x12_sensors_set_delay;
+	smdk4x12_sensors_device->device.poll = smdk4x12_sensors_poll;
+	smdk4x12_sensors_device->handlers = smdk4x12_sensors_handlers;
+	smdk4x12_sensors_device->handlers_count = smdk4x12_sensors_handlers_count;
+	smdk4x12_sensors_device->poll_fds = (struct pollfd *)
+		calloc(1, smdk4x12_sensors_handlers_count * sizeof(struct pollfd));
 
 	p = 0;
-	for (i = 0; i < exynos_sensors_handlers_count; i++) {
-		if (exynos_sensors_handlers[i] == NULL || exynos_sensors_handlers[i]->init == NULL)
+	for (i = 0; i < smdk4x12_sensors_handlers_count; i++) {
+		if (smdk4x12_sensors_handlers[i] == NULL || smdk4x12_sensors_handlers[i]->init == NULL)
 			continue;
 
-		exynos_sensors_handlers[i]->init(exynos_sensors_handlers[i], exynos_sensors_device);
-		if (exynos_sensors_handlers[i]->poll_fd >= 0) {
-			exynos_sensors_device->poll_fds[p].fd = exynos_sensors_handlers[i]->poll_fd;
-			exynos_sensors_device->poll_fds[p].events = POLLIN;
+		smdk4x12_sensors_handlers[i]->init(smdk4x12_sensors_handlers[i], smdk4x12_sensors_device);
+		if (smdk4x12_sensors_handlers[i]->poll_fd >= 0) {
+			smdk4x12_sensors_device->poll_fds[p].fd = smdk4x12_sensors_handlers[i]->poll_fd;
+			smdk4x12_sensors_device->poll_fds[p].events = POLLIN;
 			p++;
 		}
 	}
 
-	exynos_sensors_device->poll_fds_count = p;
+	smdk4x12_sensors_device->poll_fds_count = p;
 
-	*device = &(exynos_sensors_device->device.common);
+	*device = &(smdk4x12_sensors_device->device.common);
 
 	return 0;
 }
 
-int exynos_sensors_get_sensors_list(struct sensors_module_t* module,
+int smdk4x12_sensors_get_sensors_list(struct sensors_module_t* module,
 	const struct sensor_t **sensors_p)
 {
 	ALOGD("%s(%p, %p)", __func__, module, sensors_p);
@@ -281,12 +277,12 @@ int exynos_sensors_get_sensors_list(struct sensors_module_t* module,
 	if (sensors_p == NULL)
 		return -EINVAL;
 
-	*sensors_p = exynos_sensors;
-	return exynos_sensors_count;
+	*sensors_p = smdk4x12_sensors;
+	return smdk4x12_sensors_count;
 }
 
-struct hw_module_methods_t exynos_sensors_module_methods = {
-	.open = exynos_sensors_open,
+struct hw_module_methods_t smdk4x12_sensors_module_methods = {
+	.open = smdk4x12_sensors_open,
 };
 
 struct sensors_module_t HAL_MODULE_INFO_SYM = {
@@ -295,9 +291,9 @@ struct sensors_module_t HAL_MODULE_INFO_SYM = {
 		.version_major = 1,
 		.version_minor = 0,
 		.id = SENSORS_HARDWARE_MODULE_ID,
-		.name = "Exynos Sensors",
+		.name = "SMDK4x12 Sensors",
 		.author = "Paul Kocialkowski",
-		.methods = &exynos_sensors_module_methods,
+		.methods = &smdk4x12_sensors_module_methods,
 	},
-	.get_sensors_list = exynos_sensors_get_sensors_list,
+	.get_sensors_list = smdk4x12_sensors_get_sensors_list,
 };
